1ac2910f6972ee84b9d4aed9c2f62ac0487f26cd
bbouley <b.bouley@hotmail.co.uk>
2015-10-16 10:37:01 -0600

    added tests and yml file

---
+++ test/test-server.js
@@ -0,0 +1,43 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server/app');
+var should = chai.should();
+chai.use(chaiHttp);
+
+
+describe('Routes', function (){
+
+  it('should get a 200 on hitting the main route', function (done){
+    chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(200);
+      done();
+    });
+  });
+
+  it('should have a status 404', function(done){
+    chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(404);
+      // done();
+    });
+  });
+
+});
+
+// describe()
+
+// describe('Routes', function (){
+
+//   it('should get a 200 on hitting the main route', function (done){
+//     chai.request(server)
+//     .get('/')
+//     .end(function(err, res){
+//       res.should.have.status(404);
+//       done();
+//     });
+//   });
+
+// });
